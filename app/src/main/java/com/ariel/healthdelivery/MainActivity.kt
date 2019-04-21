45a5978bbab110ab382d4e2b57dd179c0df924c9
arielmarinho <ariel.marinho@blueshift.com.br>
2019-04-21 11:49:25 -0300

    adicionando dependencias e tela de localiizacao

--- app/src/main/java/com/ariel/healthdelivery/MainActivity.kt
+++ app/src/main/java/com/ariel/healthdelivery/MainActivity.kt
@@ -6,17 +6,38 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AlertDialog
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.ListAdapter
+import android.widget.RelativeLayout
 import android.widget.Toast
+import com.ariel.healthdelivery.model.Order
+
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
+
     val mAuth = FirebaseAuth.getInstance()
+    var mDatabase = FirebaseDatabase.getInstance().getReference("Order")
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val orders: ArrayList<String> = ArrayList()
+
+
+
+
+
+
+
 
         val navigationView = nav as BottomNavigationView
         navigationView.setOnNavigationItemSelectedListener { item ->
@@ -52,4 +73,8 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
 
     }
+
+
+
 }
+
